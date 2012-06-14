15ca1445afe45907fa4bda8ac909d4456c8f58ed
Roland van der Linden <Roland van der Linden@145.94.37.92>
2012-06-14 09:57:26 +0200

    [Removed] .jar from .gitignore. [Added] affectbutton library + availability test in main.

--- src/application/TestMain.java
+++ src/application/TestMain.java
@@ -1,11 +1,13 @@
 package application;
 
+import org.tudelft.affectbutton.AffectButton;
+
 
 public class TestMain
 {
 	public static void main(String[] args)
 	{
 		System.out.println("test");
-		//AffectButton affectButton = new AffectButton();
+		AffectButton affectButton = new AffectButton();
 	}
 }
