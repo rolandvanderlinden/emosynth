272a09a92db261746b78dc833884201f476871b5
Roland van der Linden <Roland van der Linden@145.94.37.92>
2012-06-14 09:48:13 +0200

    [Added] affectbutton library + availability test in main

--- src/application/TestMain.java
+++ src/application/TestMain.java
@@ -1,9 +1,12 @@
 package application;
 
+import org.tudelft.affectbutton.AffectButton;
+
 public class TestMain
 {
 	public static void main(String[] args)
 	{
 		System.out.println("test");
+		AffectButton affectButton = new AffectButton();
 	}
 }
