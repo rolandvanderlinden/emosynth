648e22a6c8735b9136dc5efdae4209da1ba1121b
Roland van der Linden <Roland van der Linden@145.94.37.92>
2012-06-14 09:51:34 +0200

    [Removed] affectbutton library due to git ignore...

--- src/application/TestMain.java
+++ src/application/TestMain.java
@@ -1,12 +1,11 @@
 package application;
 
-import org.tudelft.affectbutton.AffectButton;
 
 public class TestMain
 {
 	public static void main(String[] args)
 	{
 		System.out.println("test");
-		AffectButton affectButton = new AffectButton();
+		//AffectButton affectButton = new AffectButton();
 	}
 }
